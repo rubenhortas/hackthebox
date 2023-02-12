dbf65712e205a744b941775286d3e81e4da2fc25
rubenhortas <rubenhortas@gmail.com>
2023-02-12 20:03:38 +0100

    added

---
+++ nuclearSale/nuclear_sale_decrypt.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Mail ciphertexts are hex strings of the same length
+
+# He is a high profile individual. His information is encrypted below -> pt ^ k
+CT1_HEX = '6b65813f4fe991efe2042f79988a3b2f2559d358e55f2fa373e53b1965b5bb2b175cf039'
+
+# Here is the ciphertext encrypted with our key -> pt ^ k ^ k1
+CT2_HEX = 'fd034c32294bfa6ab44a28892e75c4f24d8e71b41cfb9a81a634b90e6238443a813a3d34'
+
+# Encrypting again with our key... -> pt ^ k1
+CT3_HEX = 'de328f76159108f7653a5883decb8dec06b0fd9bc8d0dd7dade1f04836b8a07da20bfe70'
+
+if __name__ == '__main__':
+    # CT1_HEX = pt ^ k
+    # CT2_HEX = pt ^ k ^ k1
+    # CT3_HEX = pt ^ k1
+
+    #   CT1_HEX ^ CT2_HEX =
+    # = pt ^ k ^ pt ^ k ^ k1 =
+    # = ((pt ^ k ) ^ (pt ^ k)) ^ k1 =
+    # = 0 ^ k1 =
+    # = k1
+
+    #   CT1_HEX ^ CT2_HEX ^ CT3_HEX =
+    # = (CT1_HEX ^ CT2_HEX) ^ CT3_HEX =
+    # = k1 ^ CT3_HEX =
+    # = k1 ^ pt ^ k1 =
+    # = (k1 ^ k1) ^ pt =
+    # = 0 ^ pt =
+    # = pt
+    ct1_bytes = bytes.fromhex(CT1_HEX)
+    ct2_bytes = bytes.fromhex(CT2_HEX)
+    ct3_bytes = bytes.fromhex(CT3_HEX)
+    pt_list = []
+
+    for i in range(0, len(ct1_bytes)):
+        pt_list.append(ct1_bytes[i] ^ ct2_bytes[i] ^ ct3_bytes[i])
+
+    pt_bytes = bytes(pt_list)
+    pt = pt_bytes.decode()
+    print(pt)
---
+++ optimusPrime/Decrypticon.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+from gmpy2 import gcd
+from Crypto.Util.number import inverse, long_to_bytes
+
+E_VALUES = [3, 65537]
+
+
+def _are_coprimes(a, b):
+    return gcd(a, b) == 1
+
+
+def _get_integer(name):
+    n = 0
+
+    while n == 0:
+        try:
+            n = int(input(f'Enter {name}: '))
+        except:
+            pass
+
+    return n
+
+
+def _decrypt(n1, n2, c2):
+    # n1 = p1 * q1
+    # n2 = p2 * q2
+    # If n1 and n2 are not coprimes we can find a common prime and
+    # n1 = p * q1
+    # n2 = p * q2
+    p = int(gcd(n1, n2))
+
+    # n2 = p * q2 -> q2 = n2 / p
+    q2 = n2 // p
+
+    # phi2 = (p - 1) * (q2 - 1)
+    phi2 = (p - 1) * (q2 - 1)
+
+    for e in E_VALUES:
+        # d2 = e^-1 mod(phi2)
+        d2 = inverse(e, phi2)
+
+        # p2 = c2^d2 mod(n2)
+        p2 = pow(c2, d2, n2)
+
+        p2_bytes = long_to_bytes(p2)
+
+        try:
+            pt2 = p2_bytes.decode()
+            print('e: ', e)
+            print('private key (int): ', p2)
+            print('private key (plaintext): ', pt2)
+            exit()
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    print('Get the first public key and encrypted password and close the connection')
+    print('Get the second public key and encrypted password and let open the connection to input the private key')
+
+    n1 = _get_integer('Public key 1')
+    c1 = _get_integer('Encrypted password 1')
+    n2 = _get_integer('Public key 2')
+    c2 = _get_integer('Encrypted password 2')
+
+    if not _are_coprimes(n1, n2):
+        _decrypt(n1, n2, c2)
+    else:
+        print('n1 and n2 are coprimes. Unable to decrypt message.')
+        print('Try again')
